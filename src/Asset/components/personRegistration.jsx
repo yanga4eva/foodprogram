e6867cce87525d888bb659cd41a3414f891f1586
Fortunatus Inyang <juniorinyang@yahoo.co.uk>
2020-03-28 12:45:37 -0600

    ....

--- src/Asset/components/personRegistration.jsx
+++ src/Asset/components/personRegistration.jsx
@@ -24,12 +24,12 @@ class UserDeatils extends React.Component {
   
   insertPerson(path) {
     this.props.history.push(path)
-    console.log(this.state)
       fetch("http://fgapiproject.herokuapp.com/person/register", {method: "POST", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: this.state})
        }
 
     render() { 
     const title = "Person Registration"
+    console.log(this.state.firstname)
     return(
   <Card small className="mb-4">
     <CardHeader className="border-bottom">
